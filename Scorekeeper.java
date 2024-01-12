e10d9ab347307fe406e3ed267c5c3125262ae6a6
Iraklijokhadze <113377451+Iraklijokhadze@users.noreply.github.com>
2024-01-12 10:56:55 -0800

    done

--- Scorekeeper.java
+++ Scorekeeper.java
@@ -8,29 +8,21 @@ public class Scorekeeper
 {
     private static int deckSize;   // Represents the size of the deck
     private static int score;      // Represents the player's score
-    private static long startTime; // Represents the start time of the game
+    private static long startTime = System.currentTimeMillis(); // Represents the start time of the game
 
     public static void setDeckSize(int size) 
      {
-        deckSize = 27;
+        deckSize = size;
      }
 
     public static void updateScore() 
     {
-        score = 123; 
-        System.out.println(30 - (System.currentTimeMillis()- startTime / 1000) > 5 ? (30 - (System.currentTimeMillis()- startTime / 1000)) : 5);
+        score = 30 - (System.currentTimeMillis() - startTime) / 1000 > 5 ? (int) (30 - (System.currentTimeMillis() - startTime) / 1000) : 10; 
+        startTime = System.currentTimeMillis();
     }
     
     public static int getScore() 
       {
         return score;
     }
-
-    public static void setStartTime() 
-    {
-         startTime = System.currentTimeMillis();
-    }
 }
-    
-
- 
